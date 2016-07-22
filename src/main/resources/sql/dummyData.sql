3cc5148332a8eb6b7ba41979eeb0879d14b05db1
ataian <ataian@192.108.25.70>
2016-07-22 08:23:00 +0200

    Updated dummydata to more medical texts

--- src/main/resources/sql/dummyData.sql
+++ src/main/resources/sql/dummyData.sql
@@ -1,113 +1,36 @@
-INSERT INTO "location"("name")
-VALUES ('Augmented Reality');
- 
-INSERT INTO "location"("name")
-VALUES ('Space');
- 
-INSERT INTO "location"("name")
-VALUES ('DB Bahn');
- 
-INSERT INTO "location"("name")
-VALUES ('Cloud 9');
- 
-INSERT INTO "location"("name")
-VALUES ('Disney land');
+INSERT INTO location(id, name) VALUES (1, 'Biobank Hamburg');
+INSERT INTO location(id, name) VALUES (2, 'Biobank Berlin');
+INSERT INTO location(id, name) VALUES (3, 'Biobank München');
+INSERT INTO location(id, name) VALUES (4, 'Biobank Münster');
+INSERT INTO location(id, name) VALUES (5, 'Biobank Mainz');
+INSERT INTO location(id, name) VALUES (6, 'Biobank Heidelberg');
 
-INSERT INTO "location"("name")
-VALUES ('Nirvana');
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (1, 'RESEARCHER', 'https://auth.samply.de/users/55', 'Dr.med. Harald Researcher', 'test1@test1.org', NULL, NULL);
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (2, 'OWNER', 'https://auth.samply.de/users/17', 'Goerge Biobanker, M.D.', 'test2@test2.org', NULL, 4);
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (3, 'RESEARCHER', 'https://auth.samply.de/users/007', 'Dr.med. Sean Researchington', 'test3@test3.org', NULL, NULL);
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (4, 'RESEARCHER', 'https://auth.samply.de/users/99', 'Travis Research, M.D.', 'test4@test4.org', NULL, NULL);
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (5, 'OWNER', 'https://auth-dev.mitro.dkfz.de/users/7', 'BBMRI Biobank Owner', 'owner.bbmri@bbmri.org', NULL, 5);
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (6, 'RESEARCHER', 'https://auth-dev.mitro.dkfz.de/users/8', 'BBMRI Researcher', 'researcher.bbmri@bbmri.org', NULL, NULL);
+INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (7, 'OWNER', 'https://auth-dev.mitro.dkfz.de/users/2', 'Max Ataian', 'm.ataian@dkfz.de', NULL, 6);
 
-INSERT INTO "person"(person_type, auth_subject, auth_name, auth_email, person_image)
-VALUES ('RESEARCHER', 'https://auth.samply.de/users/55', 'Terminator', 'test1@test1.org', null);
-		
-INSERT INTO "person"(person_type, auth_subject, auth_name, auth_email, person_image, location_id)
-VALUES ('OWNER', 'https://auth.samply.de/users/17', 'The Joker', 'test2@test2.org', null, 4);
+INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (1, 'Cancer', 'Are there biobanks with liver cancer samples that include the age at primary diagnosis and pharmacotherapy information?', '2015-01-02 00:00:00', 1);
+INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (2, 'Colon cancer', 'Do you have 50 samples on samples of Colorectal cancer as a primary diagnosis (C18.1 to C18.7)?', '2014-05-01 00:00:00', 6);
+INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (3, 'Lung cancer', 'For my research, I need information on the response to therapy on patients with lung cancer.', '2016-07-21 17:55:52.70183', 6);
 
-INSERT INTO "person"(person_type, auth_subject, auth_name, auth_email, person_image)
-VALUES ('RESEARCHER', 'https://auth.samply.de/users/007', 'James Bond', 'test3@test3.org', null);
+INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (1, 2, '2016-07-01 00:00:00');
+INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (2, 2, '2016-07-02 00:00:00');
+INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (1, 5, '2016-07-01 00:00:00');
+INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (2, 5, '2016-07-21 17:57:44.866');
 
-INSERT INTO "person"(person_type, auth_subject, auth_name, auth_email, person_image)
-VALUES ('RESEARCHER', 'https://auth.samply.de/users/99', 'Travis Bickle', 'test4@test4.org', null);
+INSERT INTO tag(id, query_id, text) VALUES (2, 2, 'Lung');
 
-INSERT INTO "person"(person_type, auth_subject, auth_name, auth_email, person_image, location_id)
-VALUES ('OWNER', 'https://auth-dev.mitro.dkfz.de/users/7', 'Jack Sparrow', 'test5@test5.org', null, 5);
+INSERT INTO tagged_query(query_id, tag_id) VALUES (1, 2);
 
-INSERT INTO "person"(person_type, auth_subject, auth_name, auth_email, person_image)
-VALUES ('RESEARCHER', 'https://auth-dev.mitro.dkfz.de/users/8', 'Don Vito Corleone', 'test6@test6.org', null);
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (1, 1, 5, '2016-01-01 00:00:00', 'I think the query is too general');
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (2, 1, 2, '2016-01-01 15:00:00', 'I agree with you on that');
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (4, 2, 2, '2015-05-01 00:00:00', 'Yes, we have it. Additionally I would recommend you to ask also for C19, C20 codes.');
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (5, 2, 6, '2015-05-01 01:11:00', 'Do you also have available FFPE – surgical material on those samples?');
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (6, 2, 5, '2015-05-01 01:00:00', 'We have about 400 samples on colorectal cancer as a primary diagnosis');
 
-INSERT INTO "query"("title", "text", query_creation_time, researcher_id)
-VALUES ('cancer','Data on all sorts of cancers', '2015-01-02 00:00:00', 1);
-
-INSERT INTO "query"("title", "text", query_creation_time, researcher_id)
-VALUES ('colon cancer','latest papers on colon cancer', '2015-01-01 00:00:00', 6);
-
-INSERT INTO "query"("title", "text", query_creation_time, researcher_id)
-VALUES ('lung cancer','No smoking data', '2014-05-01 00:00:00', 6);
-
-INSERT INTO "query"("title", "text", query_creation_time, researcher_id)
-VALUES ('lung cancer','Loads of smoking data', '2014-06-01 00:00:00', 6);
-
-INSERT INTO "query"("title", "text", query_creation_time, researcher_id)
-VALUES ('lung cancer','Second hand smoking data', '2015-06-01 00:00:00', 6);
-
-INSERT INTO "tag"(query_id, "text")
-VALUES (2,'Colon' );
-
-INSERT INTO "tag"(query_id, "text")
-VALUES (3,'Lung');
-
-INSERT INTO "tagged_query"(query_id, tag_id)
-VALUES (1, 2);
-
-INSERT INTO "tagged_query"(query_id, tag_id)
-VALUES (2, 1);
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (1, 5, '2016-01-01 00:00:00', 'I think the query is too general');
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (1, 2, '2016-01-01 00:00:00', 'I agree with you on that');
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (1, 2, '2016-01-01 00:00:00', 'Please explain more');
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (3, 2, '2015-05-01 00:00:00', 'What is your deal with lung cancer');
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (3, 6, '2015-05-01 00:00:00', 'They have been lying to you all the way. Smoking is actually good for your health');
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (3, 5, '2015-05-01 00:00:00', 'You talk like you represent the tobacco industry');
-
-INSERT INTO "comment"(query_id, person_id, comment_time, "text")
-VALUES (1, 5, '2016-02-02 00:00:00', 'I can share the data with you');
-
-INSERT INTO "flagged_query"(query_id, person_id, "flag")
-VALUES (1, 5, 's');
-
-INSERT INTO "flagged_query"(query_id, person_id, "flag")
-VALUES (2, 5, 'i');
-
-INSERT INTO "flagged_query"(query_id, person_id, "flag")
-VALUES (3, 5, 'a');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (1, 2, '2016-07-01 00:00:00');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (3, 2, '2016-07-02 00:00:00');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (2, 5, '2016-07-02 00:00:00');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (1, 5, '2016-07-01 00:00:00');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (3, 5, '2016-07-01 00:00:00');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (4, 5, '2016-08-01 00:00:00');
-
-INSERT INTO "query_person"(query_id, person_id, query_leaving_time) 
-VALUES (5, 5, '2015-08-01 00:00:00');
+INSERT INTO flagged_query(query_id, person_id, flag) VALUES (1, 5, 's');
+INSERT INTO flagged_query(query_id, person_id, flag) VALUES (2, 5, 'a');
