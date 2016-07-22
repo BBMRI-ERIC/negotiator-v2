9ccd4f8656250b0a473401fbdcfa50b42bc1e62b
ataian <ataian@192.108.25.70>
2016-07-22 08:43:29 +0200

    added sequence ids setting to last index number

--- src/main/resources/sql/dummyData.sql
+++ src/main/resources/sql/dummyData.sql
@@ -4,6 +4,7 @@ INSERT INTO location(id, name) VALUES (3, 'Biobank München');
 INSERT INTO location(id, name) VALUES (4, 'Biobank Münster');
 INSERT INTO location(id, name) VALUES (5, 'Biobank Mainz');
 INSERT INTO location(id, name) VALUES (6, 'Biobank Heidelberg');
+SELECT pg_catalog.setval('location_id_seq', 6, true);
 
 INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (1, 'RESEARCHER', 'https://auth.samply.de/users/55', 'Dr.med. Harald Researcher', 'test1@test1.org', NULL, NULL);
 INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (2, 'OWNER', 'https://auth.samply.de/users/17', 'Goerge Biobanker, M.D.', 'test2@test2.org', NULL, 4);
@@ -12,17 +13,20 @@ INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_
 INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (5, 'OWNER', 'https://auth-dev.mitro.dkfz.de/users/7', 'BBMRI Biobank Owner', 'owner.bbmri@bbmri.org', NULL, 5);
 INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (6, 'RESEARCHER', 'https://auth-dev.mitro.dkfz.de/users/8', 'BBMRI Researcher', 'researcher.bbmri@bbmri.org', NULL, NULL);
 INSERT INTO person(id, person_type, auth_subject, auth_name, auth_email, person_image, location_id) VALUES (7, 'OWNER', 'https://auth-dev.mitro.dkfz.de/users/2', 'Max Ataian', 'm.ataian@dkfz.de', NULL, 6);
+SELECT pg_catalog.setval('person_id_seq', 7, true);
 
 INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (1, 'Cancer', 'Are there biobanks with liver cancer samples that include the age at primary diagnosis and pharmacotherapy information?', '2015-01-02 00:00:00', 1);
 INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (2, 'Colon cancer', 'Do you have 50 samples on samples of Colorectal cancer as a primary diagnosis (C18.1 to C18.7)?', '2014-05-01 00:00:00', 6);
 INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (3, 'Lung cancer', 'For my research, I need information on the response to therapy on patients with lung cancer.', '2016-07-21 17:55:52.70183', 6);
+SELECT pg_catalog.setval('query_id_seq', 3, true);
 
 INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (1, 2, '2016-07-01 00:00:00');
 INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (2, 2, '2016-07-02 00:00:00');
 INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (1, 5, '2016-07-01 00:00:00');
 INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (2, 5, '2016-07-21 17:57:44.866');
 
-INSERT INTO tag(id, query_id, text) VALUES (2, 2, 'Lung');
+INSERT INTO tag(id, query_id, text) VALUES (1, 2, 'Lung');
+SELECT pg_catalog.setval('tag_id_seq', 1, true);
 
 INSERT INTO tagged_query(query_id, tag_id) VALUES (1, 2);
 
@@ -31,6 +35,7 @@ INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (2, 1, 2
 INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (4, 2, 2, '2015-05-01 00:00:00', 'Yes, we have it. Additionally I would recommend you to ask also for C19, C20 codes.');
 INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (5, 2, 6, '2015-05-01 01:11:00', 'Do you also have available FFPE – surgical material on those samples?');
 INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (6, 2, 5, '2015-05-01 01:00:00', 'We have about 400 samples on colorectal cancer as a primary diagnosis');
+SELECT pg_catalog.setval('comment_id_seq', 6, true);
 
 INSERT INTO flagged_query(query_id, person_id, flag) VALUES (1, 5, 's');
 INSERT INTO flagged_query(query_id, person_id, flag) VALUES (2, 5, 'a');
