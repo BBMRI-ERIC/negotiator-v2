b42229cb9455edfd87339dd9e1f9203f66b5899f
Saher Maqsood <s.maqsood@dkfz.de>
2016-08-22 09:29:15 +0200

    Fixed the issue with query.id and comment.id sequence in dummy data.

--- src/main/resources/sql/dummyData.sql
+++ src/main/resources/sql/dummyData.sql
@@ -20,7 +20,7 @@ INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (1
 INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (2, 'Colon cancer', 'Do you have 50 samples on samples of Colorectal cancer as a primary diagnosis (C18.1 to C18.7)?', '2014-05-01 00:00:00', 6);
 INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (3, 'Lung cancer', 'For my research, I need information on the response to therapy on patients with lung cancer.', '2016-07-21 17:55:52.70183', 6);
 INSERT INTO query(id, title, text, query_creation_time, researcher_id) VALUES (4, 'Skin cancer', 'Are there biobanks with skin cancer research data.', '2016-07-21 17:55:52.70183', 8);
-SELECT pg_catalog.setval('query_id_seq', 3, true);
+SELECT pg_catalog.setval('query_id_seq', 4, true);
 
 INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (1, 2, '2016-07-01 00:00:00');
 INSERT INTO query_person(query_id, person_id, query_leaving_time) VALUES (2, 2, '2016-07-02 00:00:00');
@@ -41,10 +41,10 @@ INSERT INTO tagged_query(query_id, tag_id) VALUES (1, 1);
 
 INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (1, 1, 5, '2016-01-01 00:00:00', 'I think the query is too general');
 INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (2, 1, 2, '2016-01-01 15:00:00', 'I agree with you on that');
-INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (4, 2, 2, '2015-05-01 00:00:00', 'Yes, we have it. Additionally I would recommend you to ask also for C19, C20 codes.');
-INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (5, 2, 6, '2015-05-01 01:11:00', 'Do you also have available FFPE – surgical material on those samples?');
-INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (6, 2, 5, '2015-05-01 01:00:00', 'We have about 400 samples on colorectal cancer as a primary diagnosis');
-SELECT pg_catalog.setval('comment_id_seq', 6, true);
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (3, 2, 2, '2015-05-01 00:00:00', 'Yes, we have it. Additionally I would recommend you to ask also for C19, C20 codes.');
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (4, 2, 6, '2015-05-01 01:11:00', 'Do you also have available FFPE – surgical material on those samples?');
+INSERT INTO comment(id, query_id, person_id, comment_time, text) VALUES (5, 2, 5, '2015-05-01 01:00:00', 'We have about 400 samples on colorectal cancer as a primary diagnosis');
+SELECT pg_catalog.setval('comment_id_seq', 5, true);
 
 INSERT INTO flagged_query(query_id, person_id, flag) VALUES (1, 5, 'STARRED');
 INSERT INTO flagged_query(query_id, person_id, flag) VALUES (2, 5, 'ARCHIVED');
