3fe4448ceceb8b8e0e8298fd9faeefcde81b7bc6
Paul Weingardt <weingardt.dev@gmail.com>
2016-10-11 16:27:09 +0200

    updated dummy data with two temporary queries

--- src/main/resources/sql/dummyData.sql
+++ src/main/resources/sql/dummyData.sql
@@ -19,30 +19,72 @@ SELECT pg_catalog.setval('person_id_seq', 9, true);
 
 INSERT INTO query(id, title, text, query_creation_time, researcher_id, json_text) VALUES(1, 'Cancer', 'Are there biobanks with liver cancer samples that include the age at primary diagnosis and pharmacotherapy information?', '2015-01-02 00:00:00', 1, 
 '{
-    "Country": "Germany",
-    "blood pressure": 87.86,
-    "blood sugar": 41
-}'
-);
+  "filters":{
+    "humanReadable":"name: ‘WHATEVER’, materials: ‘DNA’ or ’Plasma’"
+  },
+  "collections":[
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B0383_LTC",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    },
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B03843_LTCD",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    }
+  ]
+}');
+
 INSERT INTO query(id, title, text, query_creation_time, researcher_id, json_text) VALUES(2, 'Colon cancer', 'Do you have 50 samples on samples of Colorectal cancer as a primary diagnosis (C18.1 to C18.7)?', '2014-05-01 00:00:00', 6, 
 '{
-    "Country": "Netherlands",
-    "blood pressure": 97.0,
-    "blood sugar": 68
-}'
-);
+  "filters":{
+    "humanReadable":"name: ‘Netherlands’, materials: ’Plasma’"
+  },
+  "collections":[
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B0383_LTCH",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    },
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B03843_LTCD",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    }
+  ]
+}');
+
 INSERT INTO query(id, title, text, query_creation_time, researcher_id, json_text) VALUES(3, 'Lung cancer', 'For my research, I need information on the response to therapy on patients with lung cancer.', '2016-07-21 17:55:52.70183', 6, 
 '{
-    "Country": "Italy",
-    "blood pressure": 77.86,
-    "blood sugar": 55
+  "filters":{
+    "humanReadable":"name: ‘Germany’, materials: ‘DNA’"
+  },
+  "collections":[
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B0383_LTC",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    },
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B03843_LTCD",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    }
+  ]
 }');
+
 INSERT INTO query(id, title, text, query_creation_time, researcher_id, json_text) VALUES(4, 'Skin cancer', 'Are there biobanks with skin cancer research data.', '2016-07-21 17:55:52.70183', 8, 
 '{
-    "Country": "Spain",
-    "blood pressure": 99.86,
-    "blood sugar": 73
+  "filters":{
+    "humanReadable":"name: ‘Europe’, materials: ‘DNA’ or ’Plasma’"
+  },
+  "collections":[
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B0383_LTC",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    },
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B03843_LTCD",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    }
+  ]
 }');
+
 SELECT pg_catalog.setval('query_id_seq', 4, true);
 
 INSERT INTO query_person(query_id, person_id) VALUES (1, 2);
@@ -88,3 +130,38 @@ INSERT INTO role(role_type, person_id) VALUES ('OWNER', 9);
 INSERT INTO role(role_type, person_id) VALUES ('RESEARCHER', 9);
 
 
+INSERT INTO json_query (json_text) VALUES (
+'{
+  "filters":{
+    "humanReadable":"name: ‘Europe’, materials: ‘DNA’ or ’Plasma’"
+  },
+  "collections":[
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B0383_LTC",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    },
+    {
+      "collectionID":"bbmri-eric:collectionID:BE_B03843_LTCD",
+      "biobankID":"bbmri-eric:biobankID:BE_B0383"
+    }
+  ]
+}'
+);
+
+INSERT INTO json_query (json_text) VALUES (
+  '{
+    "filters":{
+      "humanReadable":"name: ‘Russia’"
+    },
+    "collections":[
+      {
+        "collectionID":"bbmri-eric:collectionID:BE_B0383_LTC",
+        "biobankID":"bbmri-eric:biobankID:BE_B0383"
+      },
+      {
+        "collectionID":"bbmri-eric:collectionID:BE_B03843_LTCD",
+        "biobankID":"bbmri-eric:biobankID:BE_B0383"
+      }
+    ]
+  }'
+);
