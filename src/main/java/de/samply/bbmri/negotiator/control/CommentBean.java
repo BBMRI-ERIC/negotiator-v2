9005b718091dcb21dd09f8a9518e4a5f75bc0332
Saher Maqsood <s.maqsood@dkfz.de>
2016-10-19 11:21:10 +0200

    Minimum/Maximum length of characters for comments. BIO-218 #ready

--- src/main/java/de/samply/bbmri/negotiator/control/CommentBean.java
+++ src/main/java/de/samply/bbmri/negotiator/control/CommentBean.java
@@ -42,64 +42,67 @@ import de.samply.bbmri.negotiator.jooq.tables.pojos.Query;
 @ManagedBean
 @ViewScoped
 public class CommentBean extends Observable {
-    
-    
+
     private static final long serialVersionUID = 1L;
-	
-	@ManagedProperty(value = "#{userBean}")
-	private UserBean userBean;
 
-	private String comment;
-	
+    @ManagedProperty(value = "#{userBean}")
+    private UserBean userBean;
 
-	/**
-	 * Initializes this bean by registering email notification observer
-	 */
-	@PostConstruct
-	public void init() {
-		this.addObserver(new CommentEmailNotifier());
-	}
-	
-	public UserBean getUserBean() {
-	     return userBean;
-	}
+    private String comment;
 
-	public void setUserBean(UserBean userBean) {
-	    this.userBean = userBean;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	/**
-	 * Persist comment and trigger an email notification
-	 * @param query
-	 * @return
-	 */
-	public String saveComment(Query query) {
-		DbUtil.addComment(query.getId(),  userBean.getUserId(), comment);
-    	setChanged();
-    	notifyObservers(query);
-    	
-    	return FacesContext.getCurrentInstance().getViewRoot().getViewId() + "?includeViewParams=true&faces-redirect=true";
+    /**
+     * Initializes this bean by registering email notification observer
+     */
+    @PostConstruct
+    public void init() {
+        this.addObserver(new CommentEmailNotifier());
     }
-	
-	/**
-	 * Build url to be able to navigate to the query with id=queryId
-	 * @param queryId
-	 * @return
-	 */
-	public String getQueryUrl(Integer queryId) {
-		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		
-		StringBuffer requestURL = new StringBuffer(context.getRequestServletPath());
-		requestURL.append("?queryId=").append(queryId);
-		
-		return ServletUtil.getLocalRedirectUrl(context.getRequestScheme(),context.getRequestServerName(), context.getRequestServerPort(), context.getRequestContextPath(), requestURL.toString());
-	}
+
+    /**
+     * Persist comment and trigger an email notification
+     *
+     * @param query
+     * @return
+     */
+    public String saveComment(Query query) {
+        DbUtil.addComment(query.getId(), userBean.getUserId(), comment);
+        setChanged();
+        notifyObservers(query);
+
+        return FacesContext.getCurrentInstance().getViewRoot().getViewId()
+                + "?includeViewParams=true&faces-redirect=true";
+    }
+
+    /**
+     * Build url to be able to navigate to the query with id=queryId
+     *
+     * @param queryId
+     * @return
+     */
+    public String getQueryUrl(Integer queryId) {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+
+        StringBuffer requestURL = new StringBuffer(context.getRequestServletPath());
+        requestURL.append("?queryId=").append(queryId);
+
+        return ServletUtil.getLocalRedirectUrl(context.getRequestScheme(), context.getRequestServerName(),
+                context.getRequestServerPort(), context.getRequestContextPath(), requestURL.toString());
+    }
+
+    public UserBean getUserBean() {
+        return userBean;
+    }
+
+    public void setUserBean(UserBean userBean) {
+        this.userBean = userBean;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
 }
