9555e59d3354a918a8e4e3de6e3f4fe3e7928149
s.maqsood%40dkfz-heidelberg.de <s.maqsood@dkfz-heidelberg.de>
2017-01-18 15:29:11 +0100

    Create understandable subject line for e-mail. BIO-349 #ready

--- src/main/java/de/samply/bbmri/negotiator/control/CommentEmailNotifier.java
+++ src/main/java/de/samply/bbmri/negotiator/control/CommentEmailNotifier.java
@@ -28,19 +28,16 @@ package de.samply.bbmri.negotiator.control;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
-import de.samply.bbmri.negotiator.notification.Notification;
-import de.samply.bbmri.negotiator.notification.NotificationThread;
-import de.samply.common.mailing.EmailBuilder;
-import de.samply.common.mailing.OutgoingEmail;
 import de.samply.bbmri.negotiator.Config;
 import de.samply.bbmri.negotiator.ConfigFactory;
 import de.samply.bbmri.negotiator.MailUtil;
 import de.samply.bbmri.negotiator.db.util.DbUtil;
 import de.samply.bbmri.negotiator.jooq.tables.pojos.Query;
 import de.samply.bbmri.negotiator.model.NegotiatorDTO;
+import de.samply.bbmri.negotiator.notification.Notification;
+import de.samply.bbmri.negotiator.notification.NotificationThread;
+import de.samply.common.mailing.EmailBuilder;
 
 
 /**
@@ -52,6 +49,8 @@ public class CommentEmailNotifier {
 
 	private final String url;
 
+	private static final String emailSubject = "BBMRI Negotiator: new comment on request ";
+
 	public CommentEmailNotifier(Query query, String url) {
 		this.query = query;
 		this.url = url;
@@ -61,15 +60,15 @@ public class CommentEmailNotifier {
 	 * Sends notification email when a new comment gets added to a query
 	 */
 	public void sendEmailNotification() {
-		
+
 		EmailBuilder builder = MailUtil.initializeBuilder();
 	    builder.addTemplateFile("NewCommentNotification.soy", "Notification");
 
 		Notification notification = new Notification();
-		notification.setSubject("Nothing");
+		notification.setSubject(getEmailsubject() + query.getTitle());
 
 	    try (Config config = ConfigFactory.get()) {
-	    
+
 	        List<NegotiatorDTO> negotiators = DbUtil.getPotentialNegotiators(config, query.getId());
 
 			notification.addParameter("queryName", query.getTitle());
@@ -86,4 +85,8 @@ public class CommentEmailNotifier {
             e.printStackTrace();
         }
 	}
+
+    public static String getEmailsubject() {
+        return emailSubject;
+    }
 }
