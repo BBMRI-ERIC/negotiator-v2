29268cf30f29a3c8bbccde92e892922e27188d20
Robert Reihs <robert.reihs@medunigraz.at>
2019-07-24 15:25:05 +0200

    Update DB to version V8 and cleanup V7

--- src/main/resources/db/migration/negotiator/V8__version_upgrade_db_upgrade.sql
+++ src/main/resources/db/migration/negotiator/V8__version_upgrade_db_upgrade.sql
@@ -1,3 +1,5 @@
+-- DB Upgrade to version 8
+-- Adding Networks to the data-model
 CREATE TABLE network (
     "id" SERIAL NOT NULL,
     "name" CHARACTER VARYING(255) NOT NULL,
@@ -18,3 +20,31 @@ CREATE TABLE network_collection_link (
     "network_id" INTEGER NOT NULL
 );
 CREATE INDEX "network_collection_link_index" ON "network_collection_link" ("collection_id", "network_id");
+
+-- Extending the model for attachments
+CREATE TABLE query_attachment_private (
+    "id" SERIAL NOT NULL,
+    "query_id" INTEGER NOT NULL,
+    "person_id" INTEGER NOT NULL,
+    "biobank_in_private_chat" INTEGER NOT NULL,
+    "attachment_time" TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
+    "attachment" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+-- Adding lifecycle support
+CREATE TABLE query_lifecycle_biobank (
+    "query_id" INTEGER NOT NULL,
+    "person_id" INTEGER NOT NULL,
+    "biobank_id" INTEGER NOT NULL,
+    "status" CHARACTER VARYING(255),
+    "lifecycle_time" TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL
+);
+
+CREATE TABLE query_lifecycle_collection (
+    "query_id" INTEGER NOT NULL,
+    "person_id" INTEGER NOT NULL,
+    "biobank_id" INTEGER NOT NULL,
+    "status" CHARACTER VARYING(255),
+    "lifecycle_time" TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL
+);
