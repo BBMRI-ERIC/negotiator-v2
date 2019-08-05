66dae5695b3a4dd318a685b8b83341976a6bfc4b
Robert Reihs <robert.reihs@medunigraz.at>
2019-08-05 12:18:15 +0200

    Updating for Unit testing

---
+++ src/test/java/de/samply/bbmri/negotiator/util/DataCacheTest.java
@@ -0,0 +1,18 @@
+package de.samply.bbmri.negotiator.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@DisplayName("Test DataCache")
+@ExtendWith(MockitoExtension.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DataCache.class)
+public class DataCacheTest {
+
+
+}
