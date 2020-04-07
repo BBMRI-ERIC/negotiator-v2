df00a9466e1964150083e1ef96fb3557729b81e3
Robert Reihs <robert.reihs@medunigraz.at>
2020-04-07 12:45:04 +0200

    - Update Dashboard

--- src/main/java/de/samply/bbmri/negotiator/rest/Directory.java
+++ src/main/java/de/samply/bbmri/negotiator/rest/Directory.java
@@ -32,13 +32,7 @@ import java.net.URISyntaxException;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
