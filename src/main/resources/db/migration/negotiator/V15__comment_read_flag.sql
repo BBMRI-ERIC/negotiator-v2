070708b6d65de0cf8e14bad2c254cd975225ad3f
Robert Reihs <robert.reihs@medunigraz.at>
2020-06-21 23:25:27 +0200

    - Add DB for read comments

---
+++ src/main/resources/db/migration/negotiator/V15__comment_read_flag.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public.person_comment
+(
+    person_id integer NOT NULL,
+    comment_id integer NOT NULL,
+    read character varying,
+    date_read timestamp without time zone
+);
+
+CREATE TABLE public.person_offer
+(
+    person_id integer NOT NULL,
+    offer_id integer NOT NULL,
+    read character varying,
+    date_read timestamp without time zone
+);
