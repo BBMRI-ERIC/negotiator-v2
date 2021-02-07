28b4abd15cd0f243dea951e734c8b7cc4a05b46a
Robert Reihs <robert.reihs@medunigraz.at>
2021-02-07 23:19:36 +0100

    - Add Queue Template class
    Signed-off-by: Robert Reihs <robert.reihs@medunigraz.at>

---
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/notification/util/NotificationSendQueue.java
@@ -0,0 +1,8 @@
+package eu.bbmri.eric.csit.service.negotiator.notification.util;
+
+public class NotificationSendQueue {
+
+    //https://medium.com/@cancerian0684/singleton-design-pattern-and-how-to-make-it-thread-safe-b207c0e7e368
+    //https://www.geeksforgeeks.org/java-singleton-design-pattern-practices-examples/#:~:text=To%20make%20a%20singleton%20class,can't%20access%20it%20simultaneously.&text=filter_none-,Pros%3A,It%20is%20also%20thread%20safe.
+
+}
