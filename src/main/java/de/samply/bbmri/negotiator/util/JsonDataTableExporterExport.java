1771294bef1a40d324e1938e2d55d7c1107ed51f
Robert Reihs <robert.reihs@medunigraz.at>
2021-02-24 13:40:00 +0100

    - Implement json export for admin debug issue #60

--- src/main/java/de/samply/bbmri/negotiator/util/JsonDataTableExporterExport.java
+++ src/main/java/de/samply/bbmri/negotiator/util/JsonDataTableExporterExport.java
@@ -1,94 +1,145 @@
 package de.samply.bbmri.negotiator.util;
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import org.primefaces.component.api.DynamicColumn;
 import org.primefaces.component.api.UIColumn;
 import org.primefaces.component.datatable.DataTable;
 import org.primefaces.component.datatable.export.DataTableExporter;
 import org.primefaces.component.export.ExportConfiguration;
+import org.primefaces.util.ComponentUtils;
 import org.primefaces.util.EscapeUtils;
-
-import javax.faces.FacesException;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.List;
 
 public class JsonDataTableExporterExport extends DataTableExporter {
 
-    private OutputStreamWriter osw;
-    private PrintWriter writer;
+    private StringBuilder sb;
+
+    protected StringBuilder createStringBuilder() {
+        return new StringBuilder();
+    }
+    protected StringBuilder getStringBuilder() {
+        return this.sb;
+    }
 
     @Override
-    protected void exportCells(DataTable dataTable, Object document) {
-        PrintWriter writer = (PrintWriter) document;
-        for (UIColumn col : dataTable.getColumns()) {
+    protected void doExport(FacesContext context, DataTable table, ExportConfiguration exportConfiguration, int index) throws IOException {
+
+        String pattern = "dd-MM-yyyy HH:mm";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        String date = simpleDateFormat.format(new Date());
+
+        ExternalContext externalContext = context.getExternalContext();
+        this.configureResponse(externalContext, exportConfiguration.getOutputFileName(), exportConfiguration.getEncodingType());
+        this.sb = this.createStringBuilder();
+        this.sb.append("{ 'queryTable': [");
+
+        this.exportAll(context, table, this.sb);
+
+        this.sb.append("], date: '" + date + "'}");
+        Writer writer = externalContext.getResponseOutputWriter();
+        writer.write(this.sb.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    @Override
+    protected void preRowExport(DataTable table, Object document) {
+        ((StringBuilder)document).append("\t{");
+    }
+
+    @Override
+    protected void postRowExport(DataTable table, Object document) {
+        ((StringBuilder)document).append("\t},\n");
+    }
+
+    @Override
+    protected void exportCells(DataTable table, Object document) {
+        StringBuilder builder = (StringBuilder)document;
+        Iterator columnsIterator = table.getColumns().iterator();
+
+        while(columnsIterator.hasNext()) {
+            UIColumn col = (UIColumn)columnsIterator.next();
             if (col instanceof DynamicColumn) {
-                ((DynamicColumn) col).applyStatelessModel();
+                ((DynamicColumn)col).applyStatelessModel();
             }
 
             if (col.isRendered() && col.isExportable()) {
-                String columnTag = getColumnTag(col);
-                addColumnValue(writer, col.getChildren(), columnTag, col);
+                String columnTag = this.getColumnTag(col);
+
+                try {
+                    this.addColumnValue(builder, col.getChildren(), columnTag, col);
+                } catch (IOException var8) {
+                    throw new FacesException(var8);
+                }
             }
         }
     }
 
-    @Override
-    protected void doExport(FacesContext facesContext, DataTable dataTable, ExportConfiguration exportConfiguration, int index) throws IOException {
-        writer.append("" + dataTable.getId() + "\n");
-
-        if (exportConfiguration.isPageOnly()) {
-            exportPageOnly(facesContext, dataTable, writer);
-        } else if (exportConfiguration.isSelectionOnly()) {
-            exportSelectionOnly(facesContext, dataTable, writer);
-        } else {
-            exportAll(facesContext, dataTable, writer);
-        }
-
-        writer.append("" + dataTable.getId() + "");
-
-        dataTable.setRowIndex(-1);
-    }
-
     protected String getColumnTag(UIColumn column) {
-        String headerText = (column.getExportHeaderValue() != null) ? column.getExportHeaderValue() : column.getHeaderText();
+        String headerText = column.getExportHeaderValue() != null ? column.getExportHeaderValue() : column.getHeaderText();
         UIComponent facet = column.getFacet("header");
         String columnTag;
-
         if (headerText != null) {
             columnTag = headerText.toLowerCase();
-        }
-        else if (facet != null) {
-            columnTag = exportValue(FacesContext.getCurrentInstance(), facet).toLowerCase();
-        }
-        else {
-            throw new FacesException("No suitable xml tag found for " + column);
+        } else {
+            if (!ComponentUtils.shouldRenderFacet(facet)) {
+                throw new FacesException("No suitable xml tag found for " + column);
+            }
+
+            columnTag = this.exportValue(FacesContext.getCurrentInstance(), facet).toLowerCase();
         }
 
         return EscapeUtils.forXmlTag(columnTag);
     }
 
-    protected void addColumnValue(PrintWriter writer, List<UIComponent> components, String tag, UIColumn column) {
+    protected void addColumnValue(StringBuilder builder, List<UIComponent> components, String tag, UIColumn column) throws IOException {
         FacesContext context = FacesContext.getCurrentInstance();
-
-        writer.append("\t\t" + tag + "");
-
-        if (column.getExportFunction() != null) {
-            writer.append(EscapeUtils.forXml(exportColumnByFunction(context, column)));
+        builder.append("\t\t'" + tag + "': ");
+        if(!tag.equals("jsontext")) {
+            builder.append("'");
         }
-        else {
-            for (UIComponent component : components) {
+        if (column.getExportFunction() != null) {
+            builder.append(EscapeUtils.forXml(this.exportColumnByFunction(context, column)));
+        } else {
+            Iterator iterator = components.iterator();
+
+            while(iterator.hasNext()) {
+                UIComponent component = (UIComponent)iterator.next();
                 if (component.isRendered()) {
-                    String value = exportValue(context, component);
+                    String value = this.exportValue(context, component);
                     if (value != null) {
-                        writer.append(EscapeUtils.forXml(value));
+                        if(!tag.equals("jsontext")) {
+                            builder.append(EscapeUtils.forXml(value).replaceAll("\n", "\\n"));
+                        } else {
+                            builder.append(value);
+                        }
+
                     }
                 }
             }
         }
-
-        writer.append("" + tag + "\n");
+        if(!tag.equals("jsontext")) {
+            builder.append("'");
+        }
+        builder.append(",\n");
     }
+
+    protected void configureResponse(ExternalContext externalContext, String filename, String encodingType) {
+        externalContext.setResponseContentType("application/json");
+        externalContext.setResponseHeader("Expires", "0");
+        externalContext.setResponseHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
+        externalContext.setResponseHeader("Pragma", "public");
+        externalContext.setResponseHeader("Content-disposition", ComponentUtils.createContentDisposition("attachment", filename + ".json"));
+        externalContext.addResponseCookie("primefaces.download", "true", Collections.emptyMap());
+    }
+
 }
