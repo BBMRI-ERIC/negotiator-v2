0d9201fae63b3b563c6eb2e77080b67bed95860e
RadovanTomik <tomasik@mail.muni.cz>
2022-10-21 16:08:24 +0200

    new table moderators_network

--- src/main/resources/db/migration/negotiator/V22__comment_table_add_moderator_column.sql
+++ src/main/resources/db/migration/negotiator/V22__comment_table_add_moderator_column.sql
@@ -3,3 +3,10 @@ alter table public.comment
 
 comment on column public.comment.moderated is 'marks the comment as done by user with Moderator role';
 
+create table public.moderator_network
+    (
+        person_id integer,
+        network_id integer,
+        CONSTRAINT moderator_network_pk PRIMARY KEY (person_id, network_id)
+);
+
