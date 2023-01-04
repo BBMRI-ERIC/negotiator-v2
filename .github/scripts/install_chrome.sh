247d39bde12e63cc1554ece98b11cffed9b3418f
Radovan Tomášik <tomasik@mail.muni.cz>
2023-01-04 11:04:04 +0100

    feature: acceptance test (#169)

---
+++ .github/scripts/install_chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt update
+apt install google-chrome-stable
+which chromedriver
+which google-chrome
