b7e1dd9e24f51ce66b4995d29abaa284af5df4f8
RadovanTomik <tomasik@mail.muni.cz>
2023-01-28 22:43:57 +0100

    chore: test for NotificationService

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/notification/NotificationServiceTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/notification/NotificationServiceTest.java
@@ -1,12 +1,9 @@
 package eu.bbmri.eric.csit.service.negotiator.notification;
 
-import de.samply.bbmri.negotiator.Config;
-import de.samply.bbmri.negotiator.config.Negotiator;
 import de.samply.bbmri.negotiator.jooq.tables.records.NotificationRecord;
-import eu.bbmri.eric.csit.service.negotiator.database.DatabaseUtil;
 import eu.bbmri.eric.csit.service.negotiator.database.DatabaseUtilNotification;
+import eu.bbmri.eric.csit.service.negotiator.notification.util.NotificationType;
 import org.junit.jupiter.api.Test;
-import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -14,20 +11,10 @@ import org.mockito.Mockito;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mockStatic;
 
 public class NotificationServiceTest {
-    @Mock
-    NotificationService notificationService;
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    DatabaseUtil databaseUtil;
-
-    @Mock private Config config;
-
-    @Mock private Negotiator negotiatorConfig;
-    @Mock private NotificationRecord notificationRecord;
     @Mock private DatabaseUtilNotification databaseUtilNotification;
 
 
@@ -42,7 +29,10 @@ public class NotificationServiceTest {
                     .getDatabaseUtilNotification()
                     .addNotificationEntry(Mockito.anyInt(),Mockito.anyInt(),Mockito.anyInt(),Mockito.anyInt()))
                     .thenReturn(databaseUtilNotification);
-            assertEquals(NotificationService.getDatabaseUtil().getDatabaseUtilNotification().addNotificationEntry(0,0,0,0), databaseUtilNotification);
+            NotificationService.sendNotification(NotificationType.NOT_REACHABLE_COLLECTION_NOTIFICATION,0,
+                    null, 0, map);
+            mock.verify(() -> new NotificationRecord());
+
         }
 
     }
