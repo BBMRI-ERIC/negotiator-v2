77cfd880d4268900cb2fedd5b74a96b5fce18d21
RadovanTomik <tomasik@mail.muni.cz>
2023-03-08 11:21:15 +0100

    feat: collection email parsing

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/notification/types/NotificationCollectionRegistration.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/notification/types/NotificationCollectionRegistration.java
@@ -1,8 +1,12 @@
 package eu.bbmri.eric.csit.service.negotiator.notification.types;
 
 import de.samply.bbmri.negotiator.jooq.tables.records.NotificationRecord;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -34,11 +38,16 @@ public class NotificationCollectionRegistration extends Notification{
         return collectionsList;
     }
 
-    public ArrayList<String> getContactEmailFromDirectory() throws IOException {
+    public static String getContactEmailFromDirectory(String collectionId) throws IOException, JSONException {
         HttpURLConnection connection = null;
-        URL url = new URL("https://directory.bbmri-eric.eu");
+        String contact_url = String.format("https://directory.bbmri-eric.eu/api/data/eu_bbmri_eric_persons?filter=email&q=collections.id=in=(%s)&expand=collections", collectionId);
+        URL url = new URL(contact_url);
         connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-        return new ArrayList<>();
+        String response = IOUtils.toString(connection.getInputStream(), "UTF-8");
+        JSONObject jsonObject = new JSONObject(response);
+        JSONArray jsonArray = jsonObject.getJSONArray("items");
+        JSONObject data = jsonArray.getJSONObject(0).getJSONObject("data");
+        return data.getString("email");
     }
 }
--- src/test/java/eu/bbmri/eric/csit/service/negotiator/notification/NotificationCollectionRegistrationTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/notification/NotificationCollectionRegistrationTest.java
@@ -2,9 +2,11 @@ package eu.bbmri.eric.csit.service.negotiator.notification;
 
 import de.samply.bbmri.negotiator.jooq.tables.records.NotificationRecord;
 import eu.bbmri.eric.csit.service.negotiator.notification.types.NotificationCollectionRegistration;
+import org.json.JSONException;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,12 +19,19 @@ public class NotificationCollectionRegistrationTest {
     public void testCorrectStringParsing() {
         ArrayList<String> expected = new ArrayList<>();
         expected.add("bbmri-eric:ID:DE_LMB:collection:UCCL");
+        expected.add("bbmri-eric:ID:NL_AAAACXSW447PSACQK2MBZ5YAAM:collection:AAAACXSW47ILEACQK2MBZ5YAAE");
+        expected.add("bbmri-eric:ID:ES_ivo:collection:oncologic");
         String testCollections = "(6154) bbmri-eric:ID:DE_LMB:collection:UCCL Tumorbiobank: Leipzig Medical Biobank - Tumorbiobank (1969) bbmri-eric:ID:NL_AAAACXSW447PSACQK2MBZ5YAAM:collection:AAAACXSW47ILEACQK2MBZ5YAAE BAVL58 Study of Menopause in ex-patients with Hodgkin Lymphoma: influence on long-term adverse events: Biobank+NKI - BAVL58 Study of Menopause in ex-patients with Hodgkin Lymphoma: influence on long-term adverse events (6194) bbmri-eric:ID:ES_ivo:collection:oncologic ONCOLOGIC COLLECTION: IVO BIOBANK - ONCOLOGIC COLLECTION ";
         assertEquals(expected, NotificationCollectionRegistration.parseCollectionsStringToArray(testCollections));
     }
 
     @Test
     public void testContactFromDirectory(){
-
+        try {
+            assertEquals("eva.ruckova@mou.cz", NotificationCollectionRegistration.getContactEmailFromDirectory("bbmri-eric:ID:CZ_MMCI:collection:LTS"));
+        }
+        catch (IOException | JSONException e){
+            e.printStackTrace();
+        }
     }
 }
